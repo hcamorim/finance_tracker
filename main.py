ddc8ac9a478653e1feda4a1eebcb6c3adb113175
Henrique Amorim <henriquecamposamorim@gmail.com>
2024-12-30 16:43:30 +0100

    Added two functions to plot by monthly totals and for totals by category

--- main.py
+++ main.py
@@ -33,6 +33,32 @@ def summarize_monthly_totals(df):
     monthly_totals.index = monthly_totals.index.to_timestamp()
     return monthly_totals
 
+def plot_monthly_totals(monthly_data, year):
+    """
+    Plots a line chart of monthly totals for the specified year
+    """
+    plt.figure(figsize=(10, 6))
+    plt.plot(monthly_data.index, monthly_data.values, marker='o', linestyle='-')
+    plt.title(f'Monthly Totals ({year})')
+    plt.xlabel('Month')
+    plt.ylabel('Amount')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+def plot_category_totals(category_data, year):
+    """"
+    Plots a bar chart of total amounts by category for the specified year
+    """
+    plt.figure(figsize=(12, 8))
+    plt.bar(category_data.index, category_data.values, color='skyblue')
+    plt.title(f'Totals by Category ({year})')
+    plt.xlabel('Category')
+    plt.ylabel('Amount')
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
+
 df = load_data('/Users/henriqueamorim/Henrique/finance_tracker/transactions.csv')
 
 df = add_type_column(df)
@@ -49,43 +75,21 @@ print(df.describe())
 #Setting a dataframe only for 2023
 df_2023 = filter_year(df, 2023)
 
-#Group by category and sum
-category_totals_2023 = summarize_by_category(df_2023)
-print("\nExpenses by Category:")
-print(category_totals_2023)
-
 #Group by Monthly Totals
 monthly_totals_2023 = summarize_monthly_totals(df_2023)
 print("\nMonthly Total:")
 print(monthly_totals_2023)
 
+#Group by category and sum
+category_totals_2023 = summarize_by_category(df_2023)
+print("\nExpenses by Category:")
+print(category_totals_2023)
+
 #Making the graph for monthly totals
-    ##plot(x-axis, y-axis, marker adds a circle to each data point)
-plt.plot(monthly_totals_2023.index, monthly_totals_2023.values, marker='o')
-
-    ##Names for the graph title and axis
-plt.title('Monthly Totals (2023)')
-plt.xlabel('Month')
-plt.ylabel('Amount')
-
-    ##Rotates the labels of the x-axis to improve readability
-plt.xticks(rotation=45)
-
-    ##Adjusts the layout for better fit
-plt.tight_layout()
-
-    ##Display the graph
-plt.show()
+plot_monthly_totals(monthly_totals_2023, 2023)
 
 #Making the graph for the category spending
-    ##Same graph commands as before
-plt.bar(category_totals_2023['Category'], category_totals_2023['TotalAmount'], color='skyblue')
-plt.title('Totals by Category (2023)')
-plt.xlabel('Category')
-plt.ylabel('Amount')
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
+plot_category_totals(category_totals_2023, 2023)
 
 #Seeing the differences between Income and Expenses
 totals_by_type_2023 = df_2023.groupby('Type')['Amount'].sum()
